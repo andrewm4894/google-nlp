63a9f7f5ff58270cd2066a4ad43c237dba019877
Andrew Maguire <andrewm4894@gmail.com>
2017-12-20 13:16:09 +0000

    clean up

--- run_nlp_post_parser_google_entities.sh
+++ run_nlp_post_parser_google_entities.sh
@@ -7,7 +7,7 @@ loblist=(lobA lobB lobC)
 for lob in "${loblist[@]}"
 do
 
-  python /home/andrew_maguire/localDev/codeBase/pmc-analytical-data-mart/nlp/post_parser_google_entities.py ${lob} 50 > /home/andrew_maguire/logs/run_nlp_post_parser_google_entities_${lob}_LOG_$(date "+%Y.%m.%d-%H.%M.%S").txt  2>&1&
+  python post_parser_google_entities.py ${lob} 50 > /logs/run_nlp_post_parser_google_entities_${lob}_LOG_$(date "+%Y.%m.%d-%H.%M.%S").txt  2>&1&
 
 done
 
